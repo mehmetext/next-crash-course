ac4688444a5ed21ab2cc777875c10f58ac28074d
Mehmet <mehmetext68@gmail.com>
2023-05-29 23:50:03 +0300

    added users page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 //Next
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
 	title: "Home - Next Crash Course",
 };
 
 export default function Home() {
-	return <div className="">Home</div>;
+	return (
+		<div className="">
+			<Link href="/users">Go to Users</Link>
+		</div>
+	);
 }
---
+++ src/app/users/page.tsx
@@ -0,0 +1,27 @@
+//Next
+import getAllUsers from "@/lib/getAllUsers";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+	title: "Users",
+};
+
+export default async function UsersPage() {
+	const users: User[] = await getAllUsers();
+
+	return (
+		<div className="">
+			<Link href="/" className="font-bold text-2xl">
+				Back to Home
+			</Link>
+			<div className="flex flex-col items-start">
+				{users.map((i) => (
+					<Link href={`/users/${i.id}`} key={i.id}>
+						{i.name}
+					</Link>
+				))}
+			</div>
+		</div>
+	);
+}
