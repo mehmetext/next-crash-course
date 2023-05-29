00a45170d02a3f76ce13df7496185e98850b2337
Mehmet <mehmetext68@gmail.com>
2023-05-29 22:28:13 +0300

    updated metadata and added About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+//Next
+import { Metadata } from "next";
+
+//React
+import React from "react";
+
+export const metadata: Metadata = {
+	title: "About - Next Crash Course",
+	description: "About of mehmetext",
+};
+
+export default function About() {
+	return <div>About</div>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,29 @@
+//Next
 import { Metadata } from "next";
+
+//Styles
 import "./globals.css";
+
+//Fonts
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Next Crash Course",
-	description: "mehmetext tarafından oluşturuldu.",
+	description: "Created by mehmetext",
+	generator: "mehmetext",
+	creator: "mehmetext",
+	authors: { name: "mehmetext", url: "https://konukcu.dev" },
+	keywords: ["nextjs", "crash", "course", "mehmetext"],
+	themeColor: [
+		{ media: "(prefers-color-scheme: dark)", color: "#000000" },
+		{ media: "(prefers-color-scheme: light)", color: "#ffffff" },
+	],
+	viewport: {
+		width: "device-width",
+		initialScale: 1,
+	},
 };
 
 export default function RootLayout({
@@ -15,7 +32,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="tr">
+		<html lang="en">
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
